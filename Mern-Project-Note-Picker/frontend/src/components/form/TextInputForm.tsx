4ca06124ec06be9aa7c8f61d12abbf1247b34821
BHUPENDRA SINHA <108219196+bhopusinha@users.noreply.github.com>
2024-09-27 03:33:45 -0700

    Update TextInputForm.tsx

--- Mern-Project-Note-Picker/frontend/src/components/form/TextInputForm.tsx
+++ Mern-Project-Note-Picker/frontend/src/components/form/TextInputForm.tsx
@@ -1,23 +1,41 @@
 import { Form } from "react-bootstrap";
-import { FieldError, RegisterOptions, UseFormRegister } from "react-hook-form";
+import { FieldError, RegisterOptions, useFormContext } from "react-hook-form";
+
+// interface textInputFormProp {
+//   name: string;
+//   label: string;
+//   register: UseFormRegister<any>;
+//   registeroption?: RegisterOptions;
+//   error?: FieldError;
+//   [x: string]: any;
+// }
 
 interface textInputFormProp {
   name: string;
   label: string;
-  register: UseFormRegister<any>;
   registeroption?: RegisterOptions;
-  error?: FieldError;
   [x: string]: any;
 }
 
 const TextInputForm = ({
+  // name,
+  // label,
+  // register,
+  // registeroption,
+  // error,
+  // ...props
+
   name,
   label,
-  register,
   registeroption,
-  error,
   ...props
+
 }: textInputFormProp) => {
+
+  const { register, formState: { errors } } = useFormContext();  // Using useFormContext to get form context
+  
+  const error = errors[name] as FieldError | undefined;
+
   return (
     <Form.Group className="mb-3" controlId={name + " input"}>
       <Form.Label>{label}</Form.Label>
